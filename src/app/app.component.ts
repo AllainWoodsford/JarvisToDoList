5a1e00a5c32d89e4423eb81cd013a39da06ad744
Allain Woodsford <awoodsford@hotmail.com>
2023-02-15 16:11:40 +1100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Jarvis';
+
+  
+  //State
+  todos: string[] = [];
+  todoText: string = '';
+
+  //Events
+  addTodo() {
+    this.todos.push(this.todoText)
+    localStorage.setItem('todos', JSON.stringify(this.todos));
+  }
 }
